6c77576a8b352d2973c9e30bd1ab8c0bb214b441
Khalid <C23727729@mytudublin.ie>
2025-03-13 17:36:44 +0000

    Finished at all. No bugs

--- src/AsyncWebsitePinging.java
+++ src/AsyncWebsitePinging.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,49 +9,71 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+
+/** Responsible for asynchronously pinging the website */
+
 public class AsyncWebsitePinging {
     public static void main(String[] args) throws IOException {
+        // Create a single thread that is managed by the Exceutor service. The Excecutor creates the thread
         ExecutorService servicePool = Executors.newSingleThreadExecutor();
+
+        //The website that is going to be pinged
         String websiteName = "google.com";
 
+        // create and execute to ping command with ProcessBuilder
         ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", "ping -n 10 " + websiteName);
         try {
+            //It starts the ping process that contains the ProcessBuilder
             Process process = pb.start();
+
+            // Callable task reads the process
             ContentReader readTask = new ContentReader(process);
 
+            // the future object contains the task of the executor service
             Future<List<String>> future = servicePool.submit(readTask);
 
+            // Retrives the putput after the task is completed
             List<String> output = future.get();
 
+            //Print each line
             for (String line : output)
                 System.out.println(line);
 
 
         } catch (Exception e) {
+            //Print the exception if an error occurs
             e.printStackTrace();
         } finally {
+            // Prevents new tasks from being submitted
             servicePool.shutdown();
         }
 
 
     }
 }
+/** That class implements Callable and is responsible for reading the output of the main class. It is basically a method to use.*/
     class ContentReader implements Callable<List<String>> {
-        Process process;
+
+        private Process process;//The process whose output needs to be read
+
+    //Constructor to initialise the process
         public ContentReader(Process process){
             this.process = process;
         }
+        //Method executes when the task is submitted to an ExecutorService
         public List<String> call() throws IOException {
-            List<String> output = new ArrayList<>();
+            List<String> output = new ArrayList<>();// List that stores the output from process
 
+            // BufferedReader makes the output from InputStream readable line by line
             BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String line = "";
 
+            // Read the output till there is no more information
             while ((line = br.readLine()) != null) {
-                output.add(line);
+                output.add(line);// Store each line in the list
 
             }
-            return output;
+            return output;// Return the collected output
         }
 
     }
--- src/WebsitePing.java
+++ src/WebsitePing.java
@@ -1,19 +1,27 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
+/** This class is responsible for executing a ping command 10 times and prints the output to the display*/
 public class WebsitePing {
 
     public static void main(String[] args) throws IOException {
-        String websiteName = "google.com";
+        String websiteName = "google.com"; //The website that is going to be pinged
 
+        // create and execute to ping command with ProcessBuilder
         ProcessBuilder pb = new ProcessBuilder("cmd.exe","/c", "ping -n 10 "+websiteName);
+        // Start the process to execute the ping command
         Process process = pb.start();
+        // Create a bufferedReader to read the putput from the process
         BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        String line = "";
 
+        // Each line will be hold by this variaable
+        String line = "";
+// Reads all the output from the process line by line
         while ((line = br.readLine()) != null) {
+            //Prints it to the display
             System.out.println(line);
         }
 
